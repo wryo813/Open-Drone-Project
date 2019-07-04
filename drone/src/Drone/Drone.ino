a1b70d403a1b8b7e76887f1920d0102bea3a3ab4
Ryo Watabe <wataberyo813@gmail.com>
2019-07-04 23:13:35 +0900

    memo2

--- drone/src/Drone/Drone.ino
+++ drone/src/Drone/Drone.ino
@@ -1,12 +1,27 @@
+/*
+   メモ
+   DTP603450を使ってモータを回転させると3Aでリミッターがかかって供給を停止する
+
+   優先順位
+   P_GAINの最適化
+   PID制御の実装
+   throttle_target_rawが0の時のみ電源スイッチを有効にする
+   バッテリーステータスの送信
+   高度センサ
+
+   あきらめたこと
+   カメラの実装
+   DTP603450を使うこと
+   USB使用時のモーター回転禁止
+*/
+
 #include <WiFi.h>
 #include <WiFiUDP.h>
-#include <Wire.h>
 #include <MadgwickAHRS.h>
 #include <SparkFunBQ27441.h>
 #include "serial_cmd.h"
 #include "BMX055.h"
-#include <vector>
-#include <string>
+#include "split.h"
 
 //電源管理ピン設定
 #define SHUTDOWN_PIN 25
@@ -20,9 +35,31 @@
 #define MOTOR3_PIN 4
 #define MOTOR4_PIN 14
 
+//バッテリー容量
+#define BATTERY_CAPACITY 1000
+
+/*
+   PWM設定について
+   f=1/T,T=1/fより
+   1/80[MHz]=12.5[ns]
+   PWM_FREQ[Hz]=1/(12.5[ns]×2^PWM_BIT[bit])
+*/
+//PWM周波数[Hz]
+#define PWM_FREQ 19531
+//PWM分解能[bit]
+#define PWM_BIT 12
+
 //PIDゲイン
-#define P_GAIN 1
+#define P_GAIN 10
+#define I_GAIN 1
+#define D_GAIN 1
 #define TARGET 0
+
+//yaw軸調整
+#define YAW_P_GAIN 10
+
+//スロットル調整
+#define T_GAIN 0.5
 
 //重力加速度
 #define G 9.80665
@@ -49,9 +86,6 @@ static const char *udpReturnAddr = "192.168.4.2";
 //送信機のポート
 static const int udpReturnPort = 8889;
 
-//バッテリー容量
-const unsigned int BATTERY_CAPACITY = 1000;
-
 //MadgwickFilterの出力値
 float roll = 0;
 float pitch = 0;
@@ -69,18 +103,22 @@ float dps, preDeg;
 //ループ時間計測関数用
 float loop_time, preTime;
 
+//バッテリーが接続されると、Trueになる
+bool battery_status = 0;
+
 void setup()
 {
   //GPIO入出力設定
+  //電源関連
   pinMode(PW_SWITCH_PIN, INPUT);
-  //pinMode(USB_STATUS_PIN, INPUT);
-  //pinMode(CHG_STATUS_PIN, INPUT);
+  pinMode(USB_STATUS_PIN, INPUT);
+  pinMode(CHG_STATUS_PIN, INPUT);
   pinMode(SHUTDOWN_PIN, OUTPUT);
-
-  //pinMode(MOTOR1_PIN, OUTPUT);
-  //pinMode(MOTOR2_PIN, OUTPUT);
-  //pinMode(MOTOR3_PIN, OUTPUT);
-  //pinMode(MOTOR4_PIN, OUTPUT);
+  //モーターピン
+  pinMode(MOTOR1_PIN, OUTPUT);
+  pinMode(MOTOR2_PIN, OUTPUT);
+  pinMode(MOTOR3_PIN, OUTPUT);
+  pinMode(MOTOR4_PIN, OUTPUT);
 
   //使わないピン
   pinMode(13, INPUT_PULLUP);
@@ -88,19 +126,36 @@ void setup()
   pinMode(32, INPUT_PULLUP);
   pinMode(33, INPUT_PULLUP);
 
+  digitalWrite(MOTOR1_PIN, LOW);
+  digitalWrite(MOTOR2_PIN, LOW);
+  digitalWrite(MOTOR3_PIN, LOW);
+  digitalWrite(MOTOR4_PIN, LOW);
+
+  //PWM設定
+  ledcSetup(0, PWM_FREQ, PWM_BIT);
+  ledcSetup(1, PWM_FREQ, PWM_BIT);
+  ledcSetup(2, PWM_FREQ, PWM_BIT);
+  ledcSetup(3, PWM_FREQ, PWM_BIT);
+
+  //ピンをチャンネルに接続
+  ledcAttachPin(MOTOR1_PIN, 0);
+  ledcAttachPin(MOTOR2_PIN, 1);
+  ledcAttachPin(MOTOR3_PIN, 2);
+  ledcAttachPin(MOTOR4_PIN, 3);
+
   // デバック用シリアル通信は115200bps
   Serial.begin(115200);
   Serial.println("Seial OK");
 
   //アクセスポイント構築
-  WiFi.softAP(APSSID, APPASS); //ここ消すと外部割込みが使えるようになる
+  WiFi.softAP(APSSID, APPASS);
   IPAddress myIP = WiFi.softAPIP();
   Serial.print("AP IP address: ");
   Serial.println(myIP);
   Serial.println("Wi-Fi OK");
 
   //UDPサーバ構築
-  //udp.begin(localPort);
+  udp.begin(localPort);
   Serial.println("UDP OK");
 
   //MadgwickFilterのサンプリンレート。IMUのサンプリンレートよりも小さくする5Hz(MAX25Hz)
@@ -117,20 +172,21 @@ void setup()
   setupBQ27441();
   Serial.println("battery OK");
 
-  ledcSetup(0, 12800, 8);
+  Serial.println("Setting complete");
+  Serial.print("Setting time = ");
+  Serial.print(micros());
+  Serial.println(" us");
 
-  //ピンをチャンネルに接続
-  ledcAttachPin(MOTOR1_PIN, 0);
-  delay(500);
+  delay(300);
+
+  //電源スイッチ割込み(サブルーチン関数内で一定以上ループするとリブートする)
+  // attachInterrupt(digitalPinToInterrupt(PW_SWITCH_PIN), shutdown_pw, RISING);
 
   preTime = micros();
 }
 
 void loop()
 {
-  //分割された文字列を格納する配列
-  String cmds[n] = {"\0"};
-
   int motor1_angle_target_raw = 0;
   int motor2_angle_target_raw = 0;
   int motor3_angle_target_raw = 0;
@@ -138,23 +194,35 @@ void loop()
   int angular_velocity_target_raw = 0;
   int throttle_target_raw = 0;
 
-  float motor1_angle_target;
-  float motor2_angle_target;
-  float motor3_angle_target;
-  float motor4_angle_target;
-  float angular_velocity_target;
-  float throttle_target;
+  int motor1_angle_target = 0;
+  int motor2_angle_target = 0;
+  int motor3_angle_target = 0;
+  int motor4_angle_target = 0;
+  int angular_velocity_target = 0;
+  unsigned int throttle_target = 0;
 
-  get_imu_data();
+  int motor1_duty_raw = 0;
+  int motor2_duty_raw = 0;
+  int motor3_duty_raw = 0;
+  int motor4_duty_raw = 0;
+
+  int angular_velocity = 0;
+
+  get_posture();
 
   int packetSize = udp.parsePacket();
+
   if (packetSize > 0)
   {
     int len = udp.read(packetBuffer, packetSize);
+    String cmds[n] = {"\0"};
 
     //終端文字設定
     if (len > 0)
+    {
+      //分割された文字列を格納する配列
       packetBuffer[len] = '\0';
+    }
 
     // 分割数 = 分割処理(文字列, 区切り文字, 配列)
     int index = split(packetBuffer, ',', cmds);
@@ -169,55 +237,99 @@ void loop()
     motor4_angle_target_raw = cmds[3].toInt();
     angular_velocity_target_raw = cmds[4].toInt();
     throttle_target_raw = cmds[5].toInt();
-    // Serial.println(throttle_target_raw);
   }
 
-  if (throttle_target_raw > 0)
-  {
+  angular_velocity_target = angular_velocity_target_raw * 500 / 2048;
+
+  //複数loopTime()を使うとうまくいかない
+  angular_velocity = (angular_velocity_target - degTodps(yaw)) * YAW_P_GAIN;
+
+  Serial.println(throttle_target_raw);
+
+  /*if (throttle_target_raw > 0) {
     //目標姿勢角の上限を設定
-    motor1_angle_target = motor1_angle_target_raw * 3 / 130;
+    //motor1_angle_target = motor1_angle_target_raw * 3 / 130;
+    //Serial.println(motor1_angle_target_raw);
     //スロットルをduty比に変換
-    throttle_target = throttle_target_raw / 10;
+    //throttle_target = throttle_target_raw / 10;
 
-    int motor1_duty_raw = throttle_target + (motor1_angle_target - motor1_angle_now) * P_GAIN;
+    //int motor1_duty_raw = throttle_target_raw + (motor1_angle_target_raw - motor1_angle_now) * P_GAIN;
 
-    if (motor1_duty_raw < 0)
-    {
-      motor1_duty_raw = 0;
-    }
-    ledcWrite(0, motor1_duty_raw);
-    Serial.println(motor1_duty_raw);
+    //if (motor1_duty_raw < 0) {
+    //  motor1_duty_raw = 0;
+    //}
+    // Serial.println(throttle_target_raw);
+    }*/
+
+  motor1_angle_target = motor1_angle_target_raw * 30 / 1258;
+  motor2_angle_target = motor2_angle_target_raw * 30 / 1258;
+  motor3_angle_target = motor3_angle_target_raw * 30 / 1258;
+  motor4_angle_target = motor4_angle_target_raw * 30 / 1258;
+
+  motor1_duty_raw = throttle_target_raw * T_GAIN + (motor1_angle_target - motor1_angle_now) * P_GAIN + angular_velocity;
+  motor2_duty_raw = throttle_target_raw * T_GAIN + (motor2_angle_target - motor2_angle_now) * P_GAIN - angular_velocity;
+  motor3_duty_raw = throttle_target_raw * T_GAIN + (motor3_angle_target - motor3_angle_now) * P_GAIN + angular_velocity;
+  motor4_duty_raw = throttle_target_raw * T_GAIN + (motor4_angle_target - motor4_angle_now) * P_GAIN - angular_velocity;
+
+  if (motor1_duty_raw < 0)
+    motor1_duty_raw = 0;
+  if (motor2_duty_raw < 0)
+    motor2_duty_raw = 0;
+  if (motor3_duty_raw < 0)
+    motor3_duty_raw = 0;
+  if (motor4_duty_raw < 0)
+    motor4_duty_raw = 0;
+
+  if (throttle_target_raw <= 0)
+  {
+    motor1_duty_raw = 0;
+    motor2_duty_raw = 0;
+    motor3_duty_raw = 0;
+    motor4_duty_raw = 0;
   }
+
+  /*Serial.print(motor1_duty_raw);
+    Serial.print(" ");
+    Serial.print(motor2_duty_raw);
+    Serial.print(" ");
+    Serial.print(motor3_duty_raw);
+    Serial.print(" ");
+    Serial.println(motor4_duty_raw);
+  */
+
+  //モーターノイズが入ってうまく動かない
+  /*if (digitalRead(USB_STATUS_PIN) == HIGH) {
+    motor1_duty_raw = 0;
+    motor2_duty_raw = 0;
+    motor3_duty_raw = 0;
+    motor4_duty_raw = 0;
+    }*/
+
+  ledcWrite(0, motor1_duty_raw);
+  ledcWrite(1, motor2_duty_raw);
+  ledcWrite(2, motor3_duty_raw);
+  ledcWrite(3, motor4_duty_raw);
 
   //udp.beginPacket(udpReturnAddr, udpReturnPort);
   //udp.print(printBatteryStats());
   //udp.endPacket();
   //Serial.println("loopTime = " + (String)loopTime());
+
+  /*if (digitalRead(PW_SWITCH_PIN) == HIGH) {
+    shutdown_pw();
+    }*/
 }
 
-//文字列分割関数(分割したい文字列,区切り文字,分割した文字列を代入するString配列)
-int split(String data, char delimiter, String *dst)
+void shutdown_pw()
 {
-  int index = 0;
-  int arraySize = (sizeof(data) / sizeof((data)[0]));
-  int datalength = data.length();
-  for (int i = 0; i < datalength; i++)
+  while (digitalRead(PW_SWITCH_PIN) == HIGH)
   {
-    char tmp = data.charAt(i);
-    if (tmp == delimiter)
-    {
-      index++;
-      if (index > (arraySize - 1))
-        return -1;
-    }
-    else
-      dst[index] += tmp;
   }
-  return (index + 1);
+  digitalWrite(SHUTDOWN_PIN, HIGH);
 }
 
 //IMU生値をセンサーフュージョン
-void get_imu_data()
+void get_posture()
 {
   IMU.begin_Accl();
   IMU.begin_Gyro();
@@ -239,9 +351,9 @@ void get_imu_data()
   yaw = filter.getYaw();
 
   motor1_angle_now = roll + pitch;
-  motor2_angle_now = -roll + pitch;
+  motor2_angle_now = roll - pitch;
   motor3_angle_now = -roll - pitch;
-  motor4_angle_now = roll - pitch;
+  motor4_angle_now = -roll + pitch;
 }
 
 //ループ時間計測関数
@@ -276,6 +388,7 @@ void setupBQ27441(void)
   // connected and communicating.
   if (!lipo.begin()) // begin() will return true if communication is successful
   {
+
     // If communication fails, print an error message and loop forever.
     Serial.println("Error: Unable to communicate with BQ27441.");
     Serial.println("  Check wiring and try again.");
@@ -287,7 +400,7 @@ void setupBQ27441(void)
 
   // Uset lipo.setCapacity(BATTERY_CAPACITY) to set the design capacity
   // of your battery.
-  lipo.setCapacity(BATTERY_CAPACITY);
+  battery_status = lipo.setCapacity(BATTERY_CAPACITY);
 }
 
 String printBatteryStats()
